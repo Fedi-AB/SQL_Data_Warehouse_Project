3066d8d14a715437f5502cf99228694ad1738354
Fedi ABDELLAOUI <fediabdellaoui@gmail.com>
2025-10-25 13:49:50 +0200

    Update SP_SQL_Load_Script_bronze.sql

--- scripts/1_bronze_layer/SP_SQL_Load_Script_bronze.sql
+++ scripts/1_bronze_layer/SP_SQL_Load_Script_bronze.sql
@@ -1,1 +1,18 @@
+/*
+======================================================================================
+Stored Procedure: SP_SQL_Load_Script_bronze layer (Source -> Bronze)
+======================================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates (make the tables empty) the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+        
+        ==> The SQL command BULK INSERT is used to quickly load large amounts of data 
+        from a file(example : .csv file) into a database table.
 
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+======================================================================================
+*/
