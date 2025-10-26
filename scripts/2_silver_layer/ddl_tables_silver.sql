de839433b35dac41b3a3ba96ef82e18f989ec59f
Fedi ABDELLAOUI <fediabdellaoui@gmail.com>
2025-10-26 13:39:50 +0100

    Update ddl_tables_silver.sql

--- scripts/2_silver_layer/ddl_tables_silver.sql
+++ scripts/2_silver_layer/ddl_tables_silver.sql
@@ -1,1 +1,18 @@
+/*
+===============================================================================
+DDL_TABLES_SILVER Script: We are creating the Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'silver' Tables
+===============================================================================
+*/
 
+
+/*
+===============================================================================
+--------------------Creation of crm_customer_info table------------------------
+===============================================================================
+*/
+--Checks if we have a table with the same name (Please ensure that you have a backup before excuting this line)--
