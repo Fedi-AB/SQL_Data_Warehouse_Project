3b0098eff648e8842e25bd05ba30a3313d968c4e
Fedi ABDELLAOUI <fediabdellaoui@gmail.com>
2025-10-26 14:06:22 +0100

    Update SP_SQL_Load_Script_silver.sql

--- scripts/2_silver_layer/SP_SQL_Load_Script_silver.sql
+++ scripts/2_silver_layer/SP_SQL_Load_Script_silver.sql
@@ -1,1 +1,18 @@
+/*
+===============================================================================
+Stored Procedure: SP_SQL_Load_Script_silver_layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.(make the tables empty) 
+		the silver tables before loading data to avoid duplications.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
 
+===============================================================================
+*/
